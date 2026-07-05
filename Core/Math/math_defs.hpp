27a6047de64dbc87d566054c0da6fee966779641
Vertaire Studio <vertairestudio@protonmail.com>
2026-07-05 23:48:44 +0200

    Added math related files
    Currently without any implementations. Once all basic types are implemented, these files will take next priority.

---
+++ Core/Math/math_defs.hpp
@@ -0,0 +1,10 @@
+/************************************/
+/*          math_defs.hpp           */
+/*                                  */
+/*       RatLab Game Engine         */
+/*          2026-Present            */
+/*         On MIT License           */
+/************************************/
+
+#pragma once
+
---
+++ Core/Math/math_funcs.hpp
@@ -0,0 +1,11 @@
+/************************************/
+/*         math_funcss.hpp          */
+/*                                  */
+/*       RatLab Game Engine         */
+/*          2026-Present            */
+/*         On MIT License           */
+/************************************/
+
+#pragma once
+
+#include "math_defs.hpp"
