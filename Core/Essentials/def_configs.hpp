0dc884a61d9aeae758d04b88134a4cc6c731f261
Vertaire Studio <vertairestudio@protonmail.com>
2026-07-09 19:07:05 +0200

    Safe macro initialization
    - Now the macros are ensured to be a unique macro, instead of using a pre-existing macro from a thirdparty by chance

--- Core/Essentials/def_configs.hpp
+++ Core/Essentials/def_configs.hpp
@@ -11,47 +11,50 @@
 // Implementation is inspired by Godot Engine's 'typedefs.h' file:
 // https://github.com/godotengine/godot/blob/master/core/typedefs.h
 
+// Remove already existing defines that RatLab will use, to control how it's defined manually.
+#ifdef always_inline
+#undef always_inline
+#endif
+#ifdef test_inline
+#undef test_inline
+#endif
+#ifdef func
+#undef func
+#endif
+
 // Always inlines a function, regardless of compiling configurations.
-#ifndef always_inline
-    #if defined(__GNUC__)
-        #define always_inline __attribute__((always_inline)) inline
-    #elif defined(_MSC_VER)
-        #define always_inline __forceinline
-    #else
-        #define always_inline_ inline
-    #endif
+#if defined(__GNUC__)
+    #define always_inline __attribute__((always_inline)) inline
+#elif defined(_MSC_VER)
+    #define always_inline __forceinline
+#else
+    #define always_inline_ inline
 #endif
 
 // Only inlines a function when compiling the project to a 'dev' version.
-#ifndef test_inline
-    #if defined(COMPILE_DEV)
-        #define test_inline always_inline
-    #else
-        #define test_inline
-    #endif
+#if defined(COMPILE_DEV)
+    #define test_inline always_inline
+#else
+    #define test_inline
 #endif
 
 // Will never inline a function, regardless of compiling configurations.
-#ifndef no_inline
-    #if defined(__GNUC__)
-        #define no_inline __attribute__((noinline))
-    #elif defined(_MSC_VER)
-        #define no_inline __declspec(noinline)
-    #else
-        #define no_inline
-    #endif
+#if defined(__GNUC__)
+    #define no_inline __attribute__((noinline))
+#elif defined(_MSC_VER)
+    #define no_inline __declspec(noinline)
+#else
+    #define no_inline
 #endif
 
 // Will apply either 'constexpr' or 'consteval' depending on the use case.
 // NOTE: 'constexpr' and 'consteval' is best used above C++17, otherwise no compile-time evaluation will happen.
-#ifndef func
-    #if defined(CXX) && CXX >= 20
-        #if defined(COMPILE_CONSTEVAL)
-            #define func consteval
-        #else
-            #define func constexpr
-        #endif
+#if defined(CXX) && CXX >= 20
+    #if defined(COMPILE_CONSTEVAL)
+        #define func consteval
     #else
-        #define func
+        #define func constexpr
     #endif
+#else
+    #define func
 #endif
